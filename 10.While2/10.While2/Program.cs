f391dad056324d89f1d9ccdb79c1b143c79914f9
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-08-29 15:30:36 -0500

    Add files via upload

---
+++ 10.While2/10.While2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _10.While2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un algoritmo que permita calcular la factorial de un numero dado por el usuario.(5! =1*2*3*4*5)
+            int num = 0;
+            int contador = 1;
+            int acomulador = 1;
+            Console.WriteLine("ingrese un numero para calcular la factorial");
+            num =int.Parse(Console.ReadLine());
+
+            while (contador <=num)
+            {
+               
+                acomulador = acomulador *contador;
+                Console.WriteLine($"Contador:{contador}-Factorial:{acomulador}");
+                contador++;
+
+            }
+            Console.WriteLine($"el factorial de {num}, es {acomulador}");
+
+        }
+    }
+}
---
+++ 9.CicloWhile/9.CicloWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _9.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acomulador = 0;
+
+            while (contador <= 5)
+            {
+                acomulador += contador;
+                Console.WriteLine($"c:{contador} - A:{acomulador}");
+                contador++;
+            }
+
+            Console.WriteLine($"la suma de los cinco primeros numeros enteros es: {acomulador}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
