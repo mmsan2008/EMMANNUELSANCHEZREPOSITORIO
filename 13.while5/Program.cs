940aa080a9b4257db4e3c5da40a447c41ea6baa9
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-09-03 15:33:51 -0500

    Add files via upload

---
+++ 13.while5/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _13.while5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int contarpares = 0;
+            int rango = 0;
+
+            Console.WriteLine("ingrese el rango de numeros a evaluar");
+            rango = int.Parse(Console.ReadLine());
+
+
+            while (contador <= rango)
+            {
+
+                if (contador % 2 == 0)
+                {
+                    contarpares++;
+
+                }
+                contador++;
+            }
+            Console.WriteLine($"la cantidad de numeros pares del 1 hasta {rango}, es : {contarpares}");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
