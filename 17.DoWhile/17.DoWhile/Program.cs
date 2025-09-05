fb7f9dfb82d2df7e27d60c14546797c384a08e51
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-09-05 15:42:30 -0500

    Add files via upload

---
+++ 17.DoWhile/17.DoWhile/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _17.DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*int contador = 0;
+            int acomulador = 0;
+
+            do
+            {
+                contador++;
+                acomulador = acomulador + contador;
+
+            } while (contador < 5);
+
+            Console.WriteLine($"la suma de los 5 primeros numeroses:{acomulador}");*/
+
+
+
+            //17. algoritmo que solicita un numero y genere su correspondiente tabla de multiplicar desde el 1 hasta el 10
+            //y asi sucesivamente hasta que el usuario ya no desee continuar generando tablas de multiplicar
+
+            int numtabla = 0;
+            int multiplicador = 1;
+            string respuesta = " ";
+
+
+            do
+            {   
+                Console.WriteLine("ingrese el numero de la tabla de multiplicar");
+                numtabla = int.Parse(Console.ReadLine());
+
+                do
+                {
+                    Console.WriteLine($"{numtabla} x {multiplicador} = {numtabla * multiplicador}");
+                    multiplicador++;
+
+
+
+                } while (multiplicador <= 10);
+                multiplicador = 1;
+                Console.WriteLine("desea generar otra tabla de multiplicar: s:si ,n=no");
+                respuesta = Console.ReadLine().ToLower();
+                
+
+            } while (respuesta == "s");
+
+
+
+
+
+        }
+    }
+}
---
+++ 18.Dowhile/18.Dowhile/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _18.Dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+            int acomulador = 0;
+            int saldo = 0;
+            string nombre = " ";
+            int numcuenta = 0;
+            string respuesta = " ";
+            do { 
+                Console.WriteLine("ingrese nombre\nnumero de cuenta\nsaldo\n");
+            nombre = Console.ReadLine();
+            numcuenta = int.Parse(Console.ReadLine());
+            saldo = int.Parse(Console.ReadLine());
+
+
+            if (saldo > 3000000)
+            {
+                Console.WriteLine("es apto pra el credito");
+            }
+            else if (saldo < 3000000)
+            {
+                Console.WriteLine("no es apto para el credito");
+            }
+            Console.WriteLine("quiere ingresar otro usuario: s:si n:no");
+            respuesta = Console.ReadLine().ToLower();
+
+
+        }while (respuesta == "s") ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
