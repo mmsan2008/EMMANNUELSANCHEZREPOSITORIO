a49e207faa4fef451485b0c3d06fd38051f865ab
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-09-17 13:25:55 -0500

    Add files via upload

---
+++ 21.for/21.for/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _21for;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int acumulador = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            acumulador += i;
+        }
+
+        Console.WriteLine($"La suma de los cinco primeros números enteros es {acumulador}");
+    }
+}
+
+    
+
