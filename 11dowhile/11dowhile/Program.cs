f4c9f571e50ad28971e99d863e0d10f36caff64d
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-09-17 13:24:39 -0500

    Add files via upload

---
+++ 11dowhile/11dowhile/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _11dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int suma = 0;
+
+            Console.WriteLine("ingrese numero positivo (negativo para terminar)");
+            numero = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("ingrese numero positivo (negativo para terminar)");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero > 0)
+                {
+                    suma += numero;
+                }
+
+            } while (numero >= 0);
+
+            Console.WriteLine($"la suma total es" + suma);
+
+        }
+    }
+}
---
+++ 12.dowhile/12.dowhile/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _12.dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int i = 1;
+
+            do
+            {
+                suma += i;
+                i += 2; 
+            } while (i <= 99);
+
+            Console.WriteLine("La suma de los números impares hasta 99 es: " + suma);
+        }
+    }
+}
