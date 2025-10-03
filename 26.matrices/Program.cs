48df8fd612650f887acc1d15fe54a46d290b0f2b
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-10-03 14:37:37 -0500

    Add files via upload

---
+++ 26.matrices/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _26.matrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = new int[2, 3];
+            int[,] matriz2 = new int[2, 3];
+            int[,] suma = new int[2, 3];
+
+            Console.WriteLine("Ingrese los elementos de la primera matriz (2x3):");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"matriz1[{i},{j}]: ");
+                    matriz1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Ingrese los elementos de la segunda matriz (2x3):");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"matriz2[{i},{j}]: ");
+                    matriz2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            // Sumar las matrices
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    suma[i, j] = matriz1[i, j] + matriz2[i, j];
+                }
+            }
+
+            Console.WriteLine("Matriz suma:");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(suma[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 27.matriz/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _27.matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de filas: ");
+            int filas = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de columnas: ");
+            int columnas = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[filas, columnas];
+
+            int positivos = 0, negativos = 0, ceros = 0;
+
+            Console.WriteLine("Ingrese los elementos de la matriz:");
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+
+                    if (matriz[i, j] > 0)
+                        positivos++;
+                    else if (matriz[i, j] < 0)
+                        negativos++;
+                    else
+                        ceros++;
+                }
+            }
+
+            Console.WriteLine($"\nTotal positivos: {positivos}");
+            Console.WriteLine($"Total negativos: {negativos}");
+            Console.WriteLine($"Total ceros: {ceros}");
+        }
+    }
+}
