628788f5573013a19e22de44f32f5dfbf1c9b76f
mmsan2008 <emmanuelsanchezrpo@gmail.com>
2025-10-03 15:27:19 -0500

    Add files via upload

---
+++ 28.ProgramacionModular/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _28.ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMensaje();
+            MostrarMensaje("soy Emmanuel");
+            MostrarMensaje("tengo 17 años");
+            MostrarMensaje($"tengo {calcularEdad()} años");
+            MostrarMensaje($"tengo {calcularEdad(2025, 2008)} años");
+        }
+
+        //modulo 1 - procedimiento sin parametros 
+        static void MostrarMensaje()
+        {
+            Console.WriteLine("Bienvenido a la programacion modular");
+        }
+
+        //modulo 2 - procedimiento con parametros
+        static void MostrarMensaje(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        //modulo 3 - funcion sin parametros
+
+        static int calcularEdad()
+        {
+            int edad = 2025 - 2008;
+            return edad;
+
+        }
+
+        //modulo 4 - funcion con parametros
+
+        static int calcularEdad(int añoActual, int añoNacimiento)
+        {
+            return añoActual - añoNacimiento;
+        }
+            
+
+
+
+    }
+}
---
+++ 29.Modular1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _29.Modular1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiposDe5 = new int[capturarFilas(),capturarColumnas()];
+        }
+
+
+        static int capturarFilas()
+        {
+           Console.Write("Ingrese el número de filas para la matriz");
+            int filas = int.Parse(Console.ReadLine());
+            return filas;
+        }
+
+        static int capturarColumnas()
+        {
+            Console.Write("Ingrese el número de columnas para la matriz");
+            int columnas = int.Parse(Console.ReadLine());
+           return columnas;
+        }
+
+        static int[,] llenarMatriz(int[,] matriz)
+        {
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    Console.Write($"Ingrese el valor para la posición [{i},{j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            return matriz;
+        }
+
+
+    }
+}
